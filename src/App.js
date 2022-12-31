4c71960d7cdd14cf3d5be6a1fc636efa0721772e
Ashish Ranjan <akp9801@gmail.com>
2022-12-31 21:06:51 +0530

    api call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Button from '@mui/material/Button';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AddPhotoAlternateIcon />
+        <ThreeDRotation />
+        <Button variant='contained'>Click Here</Button>
       </header>
     </div>
   );
