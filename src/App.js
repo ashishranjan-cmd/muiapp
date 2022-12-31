11fb62d933486c659a055e59868330e5c218ceb7
Ashish Ranjan <akp9801@gmail.com>
2022-12-31 22:46:56 +0530

    checkbox

--- src/App.js
+++ src/App.js
@@ -2,32 +2,64 @@ import logo from './logo.svg';
 import './App.css';
 
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
-import SaveIcon from '@mui/icons-material/Save';
+import { AccessAlarm, SafetyCheckRounded, ThreeDRotation } from '@mui/icons-material';
 
+import SaveIcon from '@mui/icons-material/Save';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CheckBox   from '@mui/icons-material/CheckBox';
+import React from 'react';
+import { FormControlLabel } from '@mui/material';
+
+function MycheckBox(){
+  // const checked = false;
+  const [ checked , setChecked ] = React.useState(false);
+
+  return(
+    <FormControlLabel 
+    control={
+      <CheckBox
+        icon={<DeleteIcon />}
+        checked = {checked}
+        onChange={(e) => setChecked(e.target.checked) }
+        inputProps={{ 'arial-label' : 'controlled'}}
+      />
+  }
+    label = "Checked"
+    />
+  )
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <MycheckBox />
+
+        <img src={logo} className="App-logo" alt="logo" /> 
+        <ButtonGroup>
         <Button 
             startIcon={ <SaveIcon /> }
-            endIcon={ <AddPhotoAlternateIcon />}
-            variant='contained'
+            // endIcon={ <AddPhotoAlternateIcon />}
+            variant='outlined'
+            color='success'
             href='#'
+            onClick={() => alert("Item Saved")}
             >
-            Save
+            Save It 
         </Button>
 
         <Button 
-            startIcon={ <SaveIcon /> }
-            endIcon={ <AddPhotoAlternateIcon />}
-            variant='contained'
+            startIcon={ <DeleteIcon /> }
+            //endIcon={ <AddPhotoAlternateIcon />}
+            variant='outlined'
+            color='error'
             href='#'
+            onClick={() => alert("Item Deleted")}
             >
-            Save
+            Delete
         </Button>
+        </ButtonGroup>
       </header>
     </div>
   );
