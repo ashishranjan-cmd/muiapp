61716cb0069c35b9f7043d981b5d7b741d3c6945
Ashish Ranjan <akp9801@gmail.com>
2022-12-31 21:12:06 +0530

    style button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import './App.css';
 import Button from '@mui/material/Button';
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+import SaveIcon from '@mui/icons-material/Save';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <AddPhotoAlternateIcon />
-        <ThreeDRotation />
-        <Button variant='contained'>Click Here</Button>
+        <Button 
+            startIcon={ <SaveIcon /> }
+            endIcon={ <AddPhotoAlternateIcon />}
+            variant='contained'
+            href='#'
+            >
+            Save
+        </Button>
+
+        <Button 
+            startIcon={ <SaveIcon /> }
+            endIcon={ <AddPhotoAlternateIcon />}
+            variant='contained'
+            href='#'
+            >
+            Save
+        </Button>
       </header>
     </div>
   );
